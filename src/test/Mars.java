26a79f4bd3b2224d0bf16c3deb77d5717dac453f
Salim Kocabas <salimkocabas@gmail.com>
2020-02-04 21:23:07 -0500

    stash den sonra ilk proje

--- src/test/Mars.java
+++ src/test/Mars.java
@@ -4,7 +4,12 @@ public class Mars {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+<<<<<<< Updated upstream
 		System.out.println("Hello Everybody");
 		
+=======
+		
+		System.out.println();
+>>>>>>> Stashed changes
 	}
 }
